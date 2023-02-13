0414ceaa00d59acb7703f66d7fcdd9d218f3669c
Juli4nHuber <juliangabrielhr@gmail.com>
2023-02-13 15:53:44 -0300

    Navbar with Bootstrap in React. Part. 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faTrophy } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedinIn, faGithub, faTwitter, faFacebook, faHtml5, faCss3, faSquareJs, faAngular, faReact, faNodeJs, faSass, faLess, faWordpress, faGulp, faGrunt, faNpm, faNode } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn, faGithub, faTwitter, faFacebook, faHtml5, faCss3, faSquareJs, faAngular, faReact, faNodeJs, faSass, faLess, faWordpress, faGulp, faGrunt, faNpm } from '@fortawesome/free-brands-svg-icons'
+import SideNav from './img/components/SideNav';
 function App() {
   
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                <span className="d-block d-lg-none">Clarence Taylor</span>
-                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="./img/profile.jpg" alt="..." /></span>
-            </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav">
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
-                </ul>
-            </div>
-        </nav>
+        <SideNav></SideNav>
         {/* <!-- Page Content--> */}
         <div className="container-fluid p-0">
             {/* <!-- About--> */}
