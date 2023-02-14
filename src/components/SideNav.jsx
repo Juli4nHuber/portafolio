ba0af973ba4ef68a1fbb570d0fc4456973caa6fd
Juli4nHuber <juliangabrielhr@gmail.com>
2023-02-14 09:32:10 -0300

    sideNav finalizado

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import {Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Profile from '../img/profile.jpg'
+import '../styles/sideNavStyles.css'
 
 export default function SideNav(){
     return (
         <div>
-            <Navbar fixed="top" expand="lg" bg="primary" className="navbar navbar-dark" id="sideNav">
+            <Navbar variant="tabs" fixed="top" expand="lg" bg="primary" className="navbar navbar-dark" id="sideNav">
                     <Navbar.Brand className="js-scroll-trigger" href="#page-top">
                         <span className="d-block d-lg-none">Clarence Taylor</span>
                         <span className="d-none d-lg-block">
                             <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={Profile} alt="..." />
                         </span>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="navbarResponsive" />
                     <Navbar.Collapse className="navbar-collapse collapse" id="navbarResponsive">
                         <Nav>
-                            <NavDropdown.Item className="nav-item" href="#about">About</NavDropdown.Item>
-                            <NavDropdown.Item className="nav-item" href="#experience">Experience</NavDropdown.Item>
-                            <NavDropdown.Item className="nav-item" href="#education">Education</NavDropdown.Item>
-                            <NavDropdown.Item className="nav-item" href="#skills">Skills</NavDropdown.Item>
-                            <NavDropdown.Item className="nav-item" href="#interests">Interests</NavDropdown.Item>
-                            <NavDropdown.Item className="nav-item" href="#awards">Awards</NavDropdown.Item>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#experience">Experience</Nav.Link>
+                            <Nav.Link href="#education">Education</Nav.Link>
+                            <Nav.Link href="#skills">Skills</Nav.Link>
+                            <Nav.Link href="#interests">Interests</Nav.Link>
+                            <Nav.Link href="#awards">Awards</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
             </Navbar>
